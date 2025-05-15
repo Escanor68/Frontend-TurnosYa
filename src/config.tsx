c81cc68286497cc96e926bda48ad3d343b443524
TImoteo Grebosz <74367166+Escanor68@users.noreply.github.com>
2025-05-15 11:53:03 -0300

    Updated config.tsx

---
+++ src/config.tsx
@@ -0,0 +1,4 @@
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://turnosya.com/api" // Replace with your production API URL
+    : "http://localhost:3001" // Your local development API URL
