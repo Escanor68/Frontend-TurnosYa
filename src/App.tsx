e5724b458600f1a65d85540fb1665b4ec49869df
TImoteo Grebosz <74367166+Escanor68@users.noreply.github.com>
2025-05-15 11:39:55 -0300

    Deploy application to Netlify

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,28 @@ import "react-toastify/dist/ReactToastify.css"
 import Header from "./components/layout/Header"
 import Footer from "./components/layout/Footer"
 
-// Pages
-import Home from "./pages/Home"
-import Fields from "./pages/Fields"
-import FieldDetails from "./pages/FieldDetails"
-import Booking from "./pages/Booking"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import UserProfile from "./pages/UserProfile"
-import AdminDashboard from "./pages/AdminDashboard"
-import AdminFieldManagement from "./pages/AdminFieldManagement"
-import ManageFields from "./pages/ManageFields"
-import NotFound from "./pages/NotFound"
+// Common pages
+import Home from "./pages/common/Home"
+import NotFound from "./pages/common/NotFound"
+
+// Auth pages
+import Login from "./pages/auth/Login"
+import Register from "./pages/auth/Register"
+
+// User pages
+import UserProfile from "./pages/user/Profile"
+
+// Admin pages
+import AdminDashboard from "./pages/admin/Dashboard"
+import AdminFieldManagement from "./pages/admin/FieldManagement"
+
+// Sports pages - Football
+import FootballFields from "./pages/sports/football/Fields"
+import FootballFieldDetails from "./pages/sports/football/FieldDetails"
+import FootballBooking from "./pages/sports/football/Booking"
+
+// Field owner pages
+import ManageFields from "./pages/field-owner/ManageFields"
 
 // Context
 import { AuthProvider } from "./context/AuthContext"
@@ -30,16 +40,29 @@ function App() {
           <Header />
           <main className="flex-grow">
             <Routes>
+              {/* Common routes */}
               <Route path="/" element={<Home />} />
-              <Route path="/fields" element={<Fields />} />
-              <Route path="/fields/:id" element={<FieldDetails />} />
-              <Route path="/booking/:fieldId" element={<Booking />} />
+
+              {/* Auth routes */}
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+
+              {/* User routes */}
               <Route path="/profile" element={<UserProfile />} />
+
+              {/* Admin routes */}
               <Route path="/admin" element={<AdminDashboard />} />
               <Route path="/admin/fields" element={<AdminFieldManagement />} />
+
+              {/* Field owner routes */}
               <Route path="/manage-fields" element={<ManageFields />} />
+
+              {/* Sports routes - Football */}
+              <Route path="/fields" element={<FootballFields />} />
+              <Route path="/fields/:id" element={<FootballFieldDetails />} />
+              <Route path="/booking/:fieldId" element={<FootballBooking />} />
+
+              {/* 404 route */}
               <Route path="*" element={<NotFound />} />
             </Routes>
           </main>
